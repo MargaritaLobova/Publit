e23bab4cc2276b925a8a7b742339dfdf60b497e1
MargaritaLobova <lobova.rita.2002@gmail.com>
2021-12-24 15:50:43 +0700

    fix ids added

--- src/main/java/com/publit/domain/Issue.java
+++ src/main/java/com/publit/domain/Issue.java
@@ -1,11 +1,13 @@
 package com.publit.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "issue")
 public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     public Issue(){
     }
 
--- src/main/java/com/publit/domain/Magazine.java
+++ src/main/java/com/publit/domain/Magazine.java
@@ -1,11 +1,13 @@
 package com.publit.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "magazine")
 public class Magazine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     public Magazine(){
     }
 }
